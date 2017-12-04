dde1d188a7fbd48247c754c27f92f522dc1d662d
Cesar Aparicio <contacto@cesaraparicio.es>
2017-12-04 19:45:22 +0100

    continuous background

--- lab/game.js
+++ lab/game.js
@@ -1,7 +1,7 @@
 function Game(canvas) {
   this.canvas = canvas;
   this.ctx = this.canvas.getContext("2d");
-  this.x = 0;
+  this.x = this.canvas.width;
   this.y = 0;
   this.img = new Image();
   this.img.src = "images/bg.png";
@@ -16,13 +16,11 @@ function Game(canvas) {
 }
 Game.prototype.draw = function() {
   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  if(this.x > this.canvas.width) {
-    this.x = 0;
+  if(this.x <= 0) {
+    this.x = this.canvas.width;
   }
   if(this.x > 0) {
     this.ctx.drawImage(this.img, - this.img.width + this.x, this.y, this.img.width, this.img.height);
-  } else if(this.x - this.img.width > 0) {
-    this.ctx.drawImage(this.img, - this.img.width * 2 + this.x, this.y, this.img.width, this.img.height);
   }
 
   this.ctx.drawImage(this.img, this.x, this.y, this.img.width, this.img.height);
