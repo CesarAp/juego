327cfbec3b1e6536edb4eb3a13adb27b43e7487c
Cesar Aparicio <contacto@cesaraparicio.es>
2017-12-05 21:18:22 +0100

    top

--- lab/game.js
+++ lab/game.js
@@ -10,33 +10,48 @@ function Game(canvas) {
   this.img.height = this.canvas.height;
       }.bind(this);
 this.elements = [];
+
+
+
 this.elements.push(new Pipe(canvas, 1120, 220, 2, 17, 17));
+this.elements.push(new Top(canvas, 1120, 220, 2, 17, 17));
 
   this.flappy = new Flappy(this.canvas);
   this.pipes =  [];
 
+  this.top = [];
+
 }
 //Preguntar al respecto de la posición de las pipes
 //porque aquí estoy seleccionando pero no posicionando
-Game.prototype.RandomPosition = function() {
-this.pipes.Math.floor(Math.random() * array.length);
-return (array[this.pipes]);
-  };
+//Game.prototype.RandomPosition = function() {
+//this.pipes.Math.floor(Math.random() * array.length);
+//return (array[this.pipes]);
+//  };
 
 Game.prototype.start = function() {
   setInterval(this.addPipe.bind(this), 2000);
   window.requestAnimationFrame(this.draw.bind(this));
 };
-
+//Game.prototype.startTwo = function() {
+  //setInterval(this.addTop.bind(this), 2000);
+  //window.requestAnimationFrame(this.draw.bind(this));
+//};
 Game.prototype.addPipe = function() {
   console.log(this.pipes.length);
   this.pipes.push(new Pipe(this.canvas, 300, 50, 100, 300));
+};
+Game.prototype.addTop = function() {
+  console.log(this.top.length);
+  this.top.push(new Top(this.canvas, 20, 50, 100, 200));
 };
 Game.prototype.draw = function() {
   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   if(this.x <= 0) {
     this.x = this.canvas.width;
   }
+
+
   if(this.x > 0) {
     this.ctx.drawImage(this.img, - this.img.width + this.x, this.y, this.img.width, this.img.height);
   }
@@ -47,6 +62,9 @@ Game.prototype.draw = function() {
 
   for (var i = 0; i < this.pipes.length; i++) {
     this.pipes[i].draw();
+  }
+  for (var j = 0; i < this.top.length; i++) {
+    this.top[i].draw();
   }
   window.requestAnimationFrame(this.draw.bind(this));
 };
@@ -70,13 +88,15 @@ Game.prototype.collide = function(elements) {
 };
 Game.prototype.fallOut = function() {
   if (this.isFalling) {
+
+
     this.y += this.speed;
   }
 };
-//Hasta aquí
+
+
 window.onload = function () {
   var canvas = document.getElementById("canvas");
   var game = new Game(canvas);
   game.start();
-
 };
--- lab/top.js
+++ lab/top.js
@@ -1,9 +1,9 @@
-function Pipe(canvas, x, y, width, height) {
+function Top(canvas, x, y, width, height) {
   this.canvas = canvas;
   this.ctx = this.canvas.getContext('2d');
 
   this.img = new Image();
-  this.img.src = "images/obstacle_bottom.png";
+  this.img.src = "images/obstacle_top.png";
   this.img.isReady = false;
   this.img.onload = function() {
     this.img.isReady = true;
@@ -15,14 +15,14 @@ function Pipe(canvas, x, y, width, height) {
   this.height = height;
 }
 // Ver si va bien con el prototype.collide de game.js
-Pipe.prototype.collide = function(element) {
-  return !(this.x + this.width < element.x ||
-    element.x + element.width < this.x ||
-    this.y + this.height < element.y ||
-    element.y + element.height < this.y);
-};
+//Top.prototype.collide = function(element) {
+  //return !(this.x + this.width < element.x ||
+    //element.x + element.width < this.x ||
+    //this.y + this.height < element.y ||
+    //element.y + element.height < this.y);
+//};
 
-Pipe.prototype.draw = function() {
+Top.prototype.draw = function() {
   if (this.img.isReady) {
     this.ctx.drawImage(this.img,
       this.x,
@@ -30,10 +30,6 @@ Pipe.prototype.draw = function() {
       this.img.width,
       this.img.height);
   }
-  this.x -= 5;
-  this.y = 550;
-
-
-
+  this.x -= 3;
 
 };
